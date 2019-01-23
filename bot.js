4f82fe6bd65cb4010b77473ac1f8a1aed79df0ca
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-23 22:46:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,32 +11,37 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
   });
   
-client.on("message", message =>{
-if(!antic[message.author.id]) {
-antic[message.author.id] = {
-actions: 0
-}}
-})
-client.on('channelDelete', alpha => {
-alpha.guild.fetchAuditLogs().then( ac => {
-var anti = ac.entries.first();
-if(anti.action == "CHANNEL_DELETE") {
-if(!antic[anti.executor.id]) {
-antic[anti.executor.id] = {
-actions: 0
-};
-} else { 
-antic[anti.executor.id].actions+=1
-if (antic[anti.executor.id].actions == 1) {
-alpha.guild.members.get(anti.executor.id).ban("Griefing")
-console.log("banned griefer 1")
-antic[anti.executor.id].actions = 0
-}
-}
-    }
-    });
-    fs.writeFile("./antic.json", JSON.stringify(antic) ,(err) =>{
-        if (err) console.log(err.message);
-    });
-});
+	let channelr = {};
+	client.on('channelDelete', async (channel) => {
+	const rebellog = client.channels.find("name", "532234981347426315"),
+	Oguild = channel.guild,
+	Onumber = 3,
+	Otime = 10000;
+	const audit = await channel.guild.fetchAuditLogs({limit: 1});
+	const channelremover = audit.entries.first().executor;
+	console.log(` A ${channel.type} Channel called ${channel.name} was deleted By ${channelremover.tag}`);
+	 if(!channelr[channelremover.id]) {
+	  channelr[channelremover.id] = {
+	  deleted : 0
+	   }
+   }
+   channelr[channelremover.id].deleted += 1;
+   if(channelr[channelremover.id].deleted >= Onumber ) {
+	Oguild.guild.roles.forEach(r => {
+		r.edit({
+			ADMINISTRATOR:  false,
+			BAN_MEMBERS: false,
+			KICK_MEMBERS: false
+					}); 
+  rebellog.send(`<@!${channelremover.id}>
+  حآول العبث بالسيرفر @everyone`);
+  channel.guild.owner.send(`<@!${channelremover.id}>
+  حآول العبث بالسيرفر ${channel.guild.name}`)
+}); 
+  }
+
+	setTimeout(() => {
+   channelr[channelremover.id].deleted = 0;
+	},Otime)
+	});
 client.login(process.env.BOT_TOKEN);

ad1acf85277cd686cda27add7b587f53a7a61466
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-24 02:13:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client(); 
 const fs = require('fs');
-const antic = JSON.parse(fs.readFileSync('./antic.json', 'utf8'));
+const antic = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
   client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('<:look:533628235922276352> <a:idk:535523806685691905> pong');
@@ -11,37 +11,35 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
   });
   
-	let channelr = {};
-	client.on('channelDelete', async (channel) => {
-	const rebellog = client.channels.find("name", "532234981347426315"),
-	Oguild = channel.guild,
-	Onumber = 3,
-	Otime = 10000;
-	const audit = await channel.guild.fetchAuditLogs({limit: 1});
-	const channelremover = audit.entries.first().executor;
-	console.log(` A ${channel.type} Channel called ${channel.name} was deleted By ${channelremover.tag}`);
-	 if(!channelr[channelremover.id]) {
-	  channelr[channelremover.id] = {
-	  deleted : 0
-	   }
-   }
-   channelr[channelremover.id].deleted += 1;
-   if(channelr[channelremover.id].deleted >= Onumber ) {
-	Oguild.guild.roles.forEach(r => {
-		r.edit({
+	client.on('channelDelete', (u) => {
+		u.guild.fetchAuditLogs().then( s => { 
+			var ss = s.entries.first();
+			if (ss.action == "CHANNEL_DELETE") {
+			if (!data[ss.executor.id]) {
+				data[ss.executor.id] = {
+				time : 1
+			  };
+		  } else {
+			  data[ss.executor.id].time+=1 
+		  };
+	data[ss.executor.id].time = 0
+	u.guild.roles.forEach(function(role) {
+		role.edit({
 			ADMINISTRATOR:  false,
 			BAN_MEMBERS: false,
 			KICK_MEMBERS: false
 					}); 
-  rebellog.send(`<@!${channelremover.id}>
-  حآول العبث بالسيرفر @everyone`);
-  channel.guild.owner.send(`<@!${channelremover.id}>
-  حآول العبث بالسيرفر ${channel.guild.name}`)
-}); 
-  }
-
-	setTimeout(() => {
-   channelr[channelremover.id].deleted = 0;
-	},Otime)
+					data[ss.executor.id].time = 0
+				});
+			setTimeout(function(){
+				if (data[ss.executor.id].time <= 3) {
+					data[ss.executor.id].time = 0
+				}
+			},60000)
+		};
+		});
+		fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+			if (err) console.log(err.message);
+		});
 	});
 client.login(process.env.BOT_TOKEN);

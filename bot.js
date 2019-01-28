0d2b327b2ef287eb193f6707b3e86e92381753c0
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-28 05:58:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
-cclient.on('channelDelete', (u) => {
+client.on('channelDelete', (u) => {
   u.guild.fetchAuditLogs().then( s => { 
       var ss = s.entries.first();
       if (ss.action == "CHANNEL_DELETE") {

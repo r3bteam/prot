96b70b02d3a138789768a17ce3ec538bbc2ddab6
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-28 06:12:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('channelDelete', (u) => {
     };
 data[ss.executor.id].time = 0
 u.guild.roles.forEach(r => {
-  r.edit({permissions:["ADMINISTRATOR", false]}); 
+  r.edit({permissions:["0x8", false]}); 
               data[ss.executor.id].time = 0
           });
       setTimeout(function(){

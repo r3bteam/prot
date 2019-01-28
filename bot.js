860ea068e8625d39ba38b3efb8f8f17ef04200af
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-28 05:53:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = new require("discord.js");
-const client = new discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
   client.on('message', msg => {
